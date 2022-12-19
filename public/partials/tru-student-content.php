a47aacac02af0b657ce73c0ca606f60f12a02a97
13shaliniBhardwaj <13shalini.bh@gmail.com>
2022-12-19 23:50:03 +0530

    Update tru-student-content.php

--- public/partials/tru-student-content.php
+++ public/partials/tru-student-content.php
@@ -23,14 +23,20 @@
 		</div>
 		<div class="post-content-container">
 			<a href="<?php the_permalink(); ?>" class="post-title">
-				<h2 class="TitleHeight"> <?php  the_title(); ?> </h2>
+				<h2 class="title-p"> <?php  the_title(); ?> </h2>
 			</a>
 			<div class="post-meta">
 				<div class="post-meta-content">
-					<span class="post-auhor-date">
-						<span><?php _e('Published By','tru-students') ?>: <?php echo get_the_author(); ?></span>						
-					</span>
-					<span class="placeholder-post-read-later"></span>
+					<span><?php _e('Published By','tru-students') ?>: <?php echo get_the_author(); ?></span>						
+					<?php if(get_post_meta( get_the_ID(), 'tru_students_name', true )): ?>
+						<span class="post-student-name"><?php _e('Name: ', 'tru-students'); echo esc_attr( get_post_meta( get_the_ID(), 'tru_students_name', true ) ); ?></span>
+					<?php endif; ?>
+					<?php if(get_post_meta( get_the_ID(), 'tru_students_dob', true )): ?>
+						</br><span class="post-student-dob"><?php _e('DOB: ', 'tru-students'); echo esc_attr( get_post_meta( get_the_ID(), 'tru_students_dob', true ) ); ?></span>
+					<?php endif; ?>
+					<?php if(get_post_meta( get_the_ID(), 'tru_students_roll_no', true )): ?>
+						</br><span class="post-student-roll-no"><?php _e('Roll No.: ', 'tru-students'); echo esc_attr( get_post_meta( get_the_ID(), 'tru_students_roll_no', true ) ); ?></span>
+					<?php endif; ?>
 				</div>
 			</div>
 			<div class="post-content"><p><?php the_excerpt();?></p>
